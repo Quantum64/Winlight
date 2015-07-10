0926b3b0b8e709fecb52c0c4c96934fe5f5da1a0
Dylan <dylan@dtr-world.com>
2015-07-09 21:40:18 -0400

    Launch4j

---
+++ WinlightMain/src/main/java/co/q64/winlight/Winlight.java
@@ -0,0 +1,15 @@
+package co.q64.winlight;
+
+public class Winlight {
+
+	private static Winlight instance;
+
+	public static void main(String[] args) {
+		instance = new Winlight();
+		instance.main();
+	}
+
+	private void main() {
+		
+	}
+}
