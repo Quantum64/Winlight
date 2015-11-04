ffee7ca368fccdc36846de2fc3f191244650d67c
Dylan <Quantum64Development@gmail.com>
2015-11-04 15:51:47 -0500

    Added repaint

---
+++ WinlightMain/src/main/java/co/q64/winlight/gui/Update.java
@@ -0,0 +1,14 @@
+package co.q64.winlight.gui;
+
+public class Update implements Runnable {
+
+	@Override
+	public void run() {
+		while (true) {
+			if (!WindowManager.getWindowManager().getWindow().isVisible()) {
+				break;
+			}
+			WindowManager.getWindowManager().getWindow().getRenderer().repaint();
+		}
+	}
+}
